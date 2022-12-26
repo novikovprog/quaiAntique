1a59efe6109bc5b4bfc7e978f295b3833a34ee39
Maxim <novikovprog@gmail.com>
2022-12-26 21:10:11 +0100

    import react router with some empty pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,44 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Menu from "./pages/Menu"
+import Single from "./pages/Single"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is home page</div>,
+  },
+  {
+    path: "/home",
+    element: <Home/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/menu",
+    element: <Menu/>,
+  },
+  {
+    path: "/single",
+    element: <Single/>,
+  },  
+]);
+
 function App() {
     return (
       <div>
-        Hello world!
+        <RouterProvider router={router} />
       </div>
     );
   }
