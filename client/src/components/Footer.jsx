33d4bedefc5484c9c37eb46912e5591b240f508e
Maxim <novikovprog@gmail.com>
2022-12-29 12:01:19 +0100

    add footer first version

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Logo from '../img/logo-footer.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={Logo} alt="" />
+      <span>Par Chef Arnaud Michaut <b>Quai Antique</b>.</span>
+    </footer>
   )
 }
 
